4ceb77309666624855523e8f1b6632db8402b14e
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-14 17:27:32 +0200

    updated store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import allReducers from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reduxThunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
-var store = createStore(allReducers, composeWithDevTools(applyMiddleware(reduxThunk)));
+var logger = createLogger();
+
+var store = createStore(allReducers, composeWithDevTools(applyMiddleware(reduxThunk, logger)));
 export default store;
