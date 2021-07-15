d43b14c14c00da1cd0c856329cb891b2a42c1d4c
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-15 11:50:42 +0200

    updated store index

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,35 @@ import allReducers from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reduxThunk from "redux-thunk";
 import { createLogger } from "redux-logger";
+//import * as actionTypes from "../constants/action-types";
 
-var logger = createLogger();
+//basic logs
+//var logger = createLogger();
+
+//collapse all logs
+// var logger = createLogger({
+//   collapsed: true
+// });
+
+//collapse only FETCH_TASKS_REQUEST
+// var logger = createLogger({
+//   collapsed: (getState, action, logEntry) => {
+//     return action.type === actionTypes.FETCH_TASKS_REQUEST
+//   }
+// });
+
+//expand only when state has "error" object
+// var logger = createLogger({
+//   collapsed: (getState, action, logEntry) => {
+//     return !logEntry.nextState.tasks.error;
+//   }
+// });
+
+var logger = createLogger({
+    predicate: (getState, action) => {
+        return process.env.REACT_APP_ENVIRONMENT === "development";
+    }
+});
 
 var store = createStore(allReducers, composeWithDevTools(applyMiddleware(reduxThunk, logger)));
 export default store;
