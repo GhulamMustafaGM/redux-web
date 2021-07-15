eb4b590b6bb09857936b21534f191dd7dd5166ac
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-15 11:55:02 +0200

    updated actions tasks

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,28 +1,68 @@
 import * as actionTypes from "../constants/action-types";
+import axios from "axios";
 
-export const fetchTasks = () => async (dispatch, getState) => {
-    var response = await fetch("http://localhost:7000/tasks", { method: "GET" });
-    var responseData = await response.json();
-    //console.log(responseData);
+export const fetchTasks = () => async (dispatch) => {
 
-    //dispatch response data to FETCH_TASKS action; it will be received by the reducer
-    dispatch({
-        type: actionTypes.FETCH_TASKS,
-        payload: responseData
-    });
+    //dispatch response data to FETCH_TASKS_REQUEST action; it will be received by the reducer
+    dispatch({ type: actionTypes.FETCH_TASKS_REQUEST });
+    try {
+        var response = await axios.get("http://localhost:7000/tasks");
+
+        //dispatch response data to FETCH_TASKS_SUCCESS action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.FETCH_TASKS_SUCCESS,
+            payload: response.data
+        });
+    }
+    catch (err) {
+        //dispatch error to FETCH_TASKS_ERROR action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.FETCH_TASKS_ERROR,
+            payload: err
+        });
+    }
 };
 
-//action creator is a function that returns an action object
-export const createTask = (newTask) => {
-    return {
-        type: actionTypes.CREATE_TASK,
-        payload: newTask
-    };
+export const createTask = (newTask) => async (dispatch) => {
+
+    //dispatch response data to CREATE_TASK_REQUEST action; it will be received by the reducer
+    dispatch({ type: actionTypes.CREATE_TASK_REQUEST });
+    try {
+        var response = await axios.post("http://localhost:7000/tasks", newTask);
+
+        //dispatch response data to CREATE_TASK_SUCCESS action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.CREATE_TASK_SUCCESS,
+            payload: response.data
+        });
+    }
+    catch (err) {
+        //dispatch error to CREATE_TASK_ERROR action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.CREATE_TASK_ERROR,
+            payload: err
+        });
+    }
 };
 
-export const deleteTask = (taskId) => {
-    return {
-        type: actionTypes.DELETE_TASK,
-        payload: taskId
+export const deleteTask = (taskId) => async (dispatch) => {
+
+    //dispatch response data to DELETE_TASK_REQUEST action; it will be received by the reducer
+    dispatch({ type: actionTypes.DELETE_TASK_REQUEST });
+    try {
+        await axios.delete(`http://localhost:7000/tasks/${taskId}`);
+
+        //dispatch response data to DELETE_TASK_SUCCESS action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.DELETE_TASK_SUCCESS,
+            payload: taskId
+        });
+    }
+    catch (err) {
+        //dispatch error to DELETE_TASK_ERROR action; it will be received by the reducer
+        dispatch({
+            type: actionTypes.DELETE_TASK_ERROR,
+            payload: err
+        });
     }
 };
