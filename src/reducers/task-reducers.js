48ced4b1e3097be18ad231016905f88c8696702c
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-15 12:26:32 +0200

    updated task-reducers

--- src/reducers/task-reducers.js
+++ src/reducers/task-reducers.js
@@ -5,37 +5,38 @@ import * as actionTypes from "../constants/action-types";
 let initialState = { data: initialTasks, loading: false, error: "" };
 
 export const tasksReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.FETCH_TASKS_REQUEST:
-            return { data: [], loading: true, error: "" };
+switch (action.type)
+{
+    case actionTypes.FETCH_TASKS_REQUEST:
+    return { data: [], loading: true, error: ""};
 
-        case actionTypes.FETCH_TASKS_SUCCESS:
-            return { data: action.payload, loading: false, error: "" };
+    case actionTypes.FETCH_TASKS_SUCCESS:
+    return { data: action.payload, loading: false, error: ""};
 
-        case actionTypes.FETCH_TASKS_ERROR:
-            return { data: state, loading: false, error: action.payload };
+    case actionTypes.FETCH_TASKS_ERROR:
+    return { data: state, loading: false, error: action.payload};
 
-        //CREATE_TASK
-        case actionTypes.CREATE_TASK_REQUEST:
-            return { data: state.data, loading: true, error: "" };
+    //CREATE_TASK
+    case actionTypes.CREATE_TASK_REQUEST:
+    return { data: state.data, loading: true, error: "" };
+    
+    case actionTypes.CREATE_TASK_SUCCESS:
+    return { data: [ ...state.data, action.payload], loading: false, error: "" };
+    
+    case actionTypes.CREATE_TASK_ERROR:
+    return { data: state.data, loading: false, error: action.payload };
 
-        case actionTypes.CREATE_TASK_SUCCESS:
-            return { data: [...state.data, action.payload], loading: false, error: "" };
-
-        case actionTypes.CREATE_TASK_ERROR:
-            return { data: state.data, loading: false, error: action.payload };
-
-        //DELETE_TASK
-        case actionTypes.DELETE_TASK_REQUEST:
-            return { data: state.data, loading: true, error: "" };
-
-        case actionTypes.DELETE_TASK_SUCCESS:
-            return { data: state.data.filter(task => task.id !== action.payload), loading: false, error: "" };
-
-        case actionTypes.DELETE_TASK_ERROR:
-            return { data: state.data, loading: false, error: action.payload };
-
-        default:
-            return state;
-    }
+    //DELETE_TASK
+    case actionTypes.DELETE_TASK_REQUEST:
+    return { data: state.data, loading: true, error: "" };
+    
+    case actionTypes.DELETE_TASK_SUCCESS:
+    return { data: state.data.filter(task => task.id !== action.payload), loading: false, error: "" };
+    
+    case actionTypes.DELETE_TASK_ERROR:
+    return { data: state.data, loading: false, error: action.payload };
+    
+    default:
+    return state;
+}
 };
